005140aaa60512b928ee97720a451a0b8ba13da9
kkarolinadrozdzal-art <k.karolinadrozdzal@gmail.com>
2025-11-04 21:15:36 +0100

    proba

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,24 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int licznik = 0;
+    int n_zaglowiec = 0;
+    while (licznik < towar) {
+
+       if (towar == 0) {
+            return 0;
+        }
+            Stocznia stocznia{};
+            Statek*  s1 = stocznia();
+        
+        licznik += s1->transportuj();
+        if (dynamic_cast<Zaglowiec*>(s1)!=nullptr) {
+            n_zaglowiec++;
+        }
+        delete[] s1;
+        if (licznik>=towar)
+        {
+            return n_zaglowiec;
+        }
+    }
 }
